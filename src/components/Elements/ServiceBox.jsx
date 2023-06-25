26c3170949d6c6122155d5bfc762d4963ad625af
gokhanaydinli <gokhanaydinli1998@gmail.com>
2023-06-25 16:33:49 +0300

    catalog pdf changed

--- src/components/Elements/ServiceBox.jsx
+++ src/components/Elements/ServiceBox.jsx
@@ -30,7 +30,6 @@ export default function ServiceBox({icon, title, subtitle}) {
 
   return (
     <Wrapper className="flex flexColumn">
-      <IconStyle >{getIcon}</IconStyle>
       <TitleStyle className="font20 extraBold">{title}</TitleStyle>
       <SubtitleStyle className="font13">{subtitle}</SubtitleStyle>
     </Wrapper>
--- src/components/Sections/Document.jsx
+++ src/components/Sections/Document.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import pdf from "../Elements/satodoor_catalog.pdf";
 import productPdf from "../Elements/satodoor_product.pdf";
+import catalog from "../Elements/katalog-celik-kapi.pdf";
 import book from "../book.svg";
 import edit from "../edit.svg";
 import video from "../video-camera.svg"
@@ -17,7 +18,7 @@ export default function Document() {
         <span className="pdf-container-title">{t("document.catalogTitle")}</span>
         <div className="pdf-container">
           <img alt='slider' style={{ width: '40px' }} src={book}></img>
-          <a href={productPdf}
+          <a href={catalog}
             target="_blank"
             rel="noreferrer">
             {t("document.catalogLink")}
@@ -28,7 +29,7 @@ export default function Document() {
         <span className="pdf-container-title">{t("document.technicalTitle")}</span>
         <div className="pdf-container">
           <img alt='slider' style={{ width: '40px' }} src={edit}></img>
-          <a href={pdf}
+          <a href={productPdf}
             target="_blank"
             rel="noreferrer">
             {t("document.technicalLink")}
@@ -39,7 +40,7 @@ export default function Document() {
         <span className="pdf-container-title">{t("document.galleryTitle")}</span>
         <div className="pdf-container">
           <img alt='slider' style={{ width: '40px' }} src={video}></img>
-          <a href={productPdf}
+          <a href={pdf}
             target="_blank"
             rel="noreferrer">
             {t("document.galleryLink")}
